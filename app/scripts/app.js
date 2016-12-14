01fc190fd6b61758d107c9587769f4f987a5ed84
thomaskdesign <thomas.kaiaua@gmail.com>
2016-12-13 21:19:25 -0800

    Setting up environment

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name 2ndAngularAppApp
+ * @description
+ * # 2ndAngularAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('2ndAngularAppApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name 2ndAngularAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the 2ndAngularAppApp
+ */
+angular.module('2ndAngularAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
